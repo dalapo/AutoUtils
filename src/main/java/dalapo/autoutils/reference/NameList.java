f05eb216107c08017c5747082c28a6eabda69e53
David <dave.powell1716@gmail.com>
2017-05-11 19:44:27 -0700

    First public release

--- src/main/java/dalapo/autoutils/reference/NameList.java
+++ src/main/java/dalapo/autoutils/reference/NameList.java
@@ -7,5 +7,5 @@ public class NameList {
 	
 	public static final String MODNAME = "Automation Utilities";
 	
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "1.0";
 }
